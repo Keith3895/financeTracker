9d95eb701211aa0d3c8545b9e4c408c281c8802d
Keith Franklin <keith30895@gmail.com>
2018-03-25 08:32:06 +0530

    linked app and backend

--- angularFiles/src/app/service/cordova/cordova.service.ts
+++ angularFiles/src/app/service/cordova/cordova.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { SystemService } from '../system.service';
 import { SmsService } from '../smsfunctions/sms.service';
 import { HttpClient } from '@angular/common/http';
-
 // declare const window: any;
 declare const cordova: any;
 declare const SMS: any;
@@ -11,61 +10,57 @@ declare const SMS: any;
 export class CordovaService {
   systemService;
   smsList = [];
+  ep;
   constructor(public smsService: SmsService,
-    public http:HttpClient) {
+    public http: HttpClient) {
     this.systemService = new SystemService();
+    this.ep = this.systemService.getURL() + '/transaction';
     document.addEventListener('onSMSArrive', (e) => {
-      
+
       var sms = e['data'];
       this.smsList.push(sms);
-      let transactions = [];
       let transactionObject = this.smsService.Transaction(sms);
       if (typeof transactionObject == 'object') {
         cordova.plugins.backgroundMode.disableWebViewOptimizations();
         cordova.plugins.locationServices.geolocation.getCurrentPosition((position) => {
-          transactions.push(Object.assign(transactionObject, { geoLocation: position.coords }));
-          // http
-        }, this.onError,{ enableHighAccuracy: true });
+          this.http.put(this.ep + '/addMany', Object.assign(transactionObject, { geoLocation: position.coords })).subscribe(res => {
+            console.log('sms sent');
+          });
+        }, this.onError, { enableHighAccuracy: true });
       }
     });
   }
-  onSuccess(position) {
-    alert('Latitude: ' + position.coords.latitude + '\n' +
-      'Longitude: ' + position.coords.longitude + '\n' +
-      'Altitude: ' + position.coords.altitude + '\n' +
-      'Accuracy: ' + position.coords.accuracy + '\n' +
-      'Altitude Accuracy: ' + position.coords.altitudeAccuracy + '\n' +
-      'Heading: ' + position.coords.heading + '\n' +
-      'Speed: ' + position.coords.speed + '\n' +
-      'Timestamp: ' + position.timestamp + '\n');
-  }
+
   onError(error) {
     alert('code: ' + error.code + '\n' +
       'message: ' + error.message + '\n');
   }
   runBackground() {
     cordova.plugins.backgroundMode.enable();
-    cordova.plugins.backgroundMode.disableWebViewOptimizations(); 
-    cordova.plugins.backgroundMode.on('activate', function() {
-      cordova.plugins.backgroundMode.disableWebViewOptimizations(); 
-   });
-   cordova.plugins.backgroundMode.on('enable', function() {
-    cordova.plugins.backgroundMode.disableWebViewOptimizations(); 
- });
+    cordova.plugins.backgroundMode.disableWebViewOptimizations();
+    cordova.plugins.backgroundMode.on('activate', function () {
+      cordova.plugins.backgroundMode.disableWebViewOptimizations();
+    });
+    cordova.plugins.backgroundMode.on('enable', function () {
+      cordova.plugins.backgroundMode.disableWebViewOptimizations();
+    });
   }
   readMessages() {
     const filter = {
       box: 'inbox',
     };
-    SMS.listSMS(filter,async (data) => {
-      let templist=[];
+    SMS.listSMS(filter, async (data) => {
+      let templist = [];
       this.smsList = data;
       await this.smsList.forEach(el => {
         let condi = this.smsService.Transaction(el)
-        if(condi)
-        templist.push(condi);
+        if (condi)
+          templist.push(condi);
       });
-      
+      this.http.put(this.ep + '/addMany', templist).subscribe(res => {
+        //popup
+        console.log('sms list sent');
+      });
     }, (err) => {
       console.log('error list sms: ' + err);
     });
@@ -86,8 +81,7 @@ export class CordovaService {
     this.requestPermision((st) => {
       SMS.startWatch((status) => {
       }, null);
-      cordova.plugins.backgroundMode.disableWebViewOptimizations(); 
-      // cordova.plugins.locationServices.geolocation.getCurrentPosition(this.onSuccess,this.onError,{ enableHighAccuracy: true });
+      cordova.plugins.backgroundMode.disableWebViewOptimizations();
     });
   }
 }
