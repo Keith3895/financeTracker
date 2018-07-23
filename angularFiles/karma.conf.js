24209d51c5f7cab372a81b5dcec8d79f963ccd92
Keith Franklin <keith30895@gmail.com>
2018-07-23 18:51:05 +0530

    should create

--- angularFiles/karma.conf.js
+++ angularFiles/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadless'],
     singleRun: true,
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
