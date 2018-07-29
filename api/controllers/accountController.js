33505763be70cf49957d073b09047ee0998ab1ec
Keith Franklin <keith30895@gmail.com>
2018-07-29 07:56:40 +0530

    added simple backend validation

--- api/controllers/accountController.js
+++ api/controllers/accountController.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 const Account = mongoose.model('Account');
-
+const validator = require('../validator');
 exports.addAccount = (req, res) => {
+    let validation = validator.validateBank(req.body);
+    if (validation.errorMessage) {
+        return res.status(500).json(validation);
+    }
     let newAccount = new Account(req.body);
     newAccount.save((err, acc) => {
         if (err) {
@@ -16,7 +20,7 @@ exports.addAccount = (req, res) => {
 exports.getAccounts = (req, res) => {
     let username = req.body.username;
     Account.find({ user: username }, (err, accounts) => {
-        if(err) {
+        if (err) {
             return res.status(400).send({
                 message: err
             });
