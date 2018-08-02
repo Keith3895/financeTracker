48393a0315ac1edd9692bf1eaa33724ff109e3bf
Ramya Krishna <ramya.krishna@neutrinos.co>
2018-08-02 14:46:37 +0530

    fixed requested issues

--- angularFiles/src/app/component/fab-button/fab-button.component.ts
+++ angularFiles/src/app/component/fab-button/fab-button.component.ts
@@ -8,14 +8,15 @@ import { Component, OnInit, Input } from '@angular/core';
 export class FabButtonComponent implements OnInit {
   @Input() flexDirection: String;
   @Input() showIcons: Boolean;
-  @Input() alignBtn: String;   // button alignment on screen
-
+  @Input() alignBtn: String;  
+  @Input() class : String;
+  @Input() icon : String;
   constructor() { }
 
   ngOnInit() {
   }
 
-  // set direction of fab to float
+  // floating action button direction
   setFlexDirection() {
     if (this.flexDirection === 'row' || this.flexDirection === 'row-reverse') {
       return 'containerRow';
