f3925d77a16d7ceb9c964dfba7a32a6413eefad7
Ramya Krishna <ramya.krishna@neutrinos.co>
2018-08-22 02:09:56 +0530

    list page view

--- angularFiles/src/app/component/list/list.component.ts
+++ angularFiles/src/app/component/list/list.component.ts
@@ -6,10 +6,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-
+  list = [{
+    "icon": "Spent.svg",
+    "Text": "Spent",
+    "Amount": 3000,
+    "Date": new Date(),
+    "action" : "fas fa-arrow-circle-right"
+  },
+  {
+    "icon": "Receive.svg",
+    "Text": "Recieved",
+    "Amount": 3000,
+    "Date": new Date(),
+    "action" : "fas fa-arrow-circle-right"
+  },{
+    "icon": "Receive.svg",
+    "Text": "Recieved",
+    "Amount": 3000,
+    "Date": new Date(),
+    "action" : "fas fa-arrow-circle-right"
+  },{
+    "icon": "Spent.svg",
+    "Text": "Spent",
+    "Amount": 3000,
+    "Date": new Date(),
+    "action" : "fas fa-arrow-circle-right"
+  },{
+    "icon": "Receive.svg",
+    "Text": "Recieved",
+    "Amount": 3000,
+    "Date": new Date(),
+    "action" : "fas fa-arrow-circle-right"
+  },{
+    "icon": "Receive.svg",
+    "Text": "received",
+    "Amount": 3000,
+    "Date": new Date(),
+    "action" : "fas fa-arrow-circle-right"
+  }]
   constructor() { }
 
   ngOnInit() {
   }
 
+  addFuntionality(){
+    
+  }
 }
